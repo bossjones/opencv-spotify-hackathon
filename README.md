22cae44637db6ce524065279898f7ce7ad34b263
Malcolm Jones <malcolm@adobe.com>
2014-11-01 16:30:10 -0400

    new: added audio files, updated README.md with brew install commands, added imgs, added opencv cmd to read in scar-girl.jpg @new

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ opencv-spotify-hackathon
 ========================
 
 Using opencv to do face detection and play a creepy sound every-time a face is detected
+
+
+## install pygame in virtualenv
+brew install libvorbis sdl sdl_image sdl_mixer sdl_ttf portmidi
+pip install hg+http://bitbucket.org/pygame/pygame
--- detect.py
+++ detect.py
@@ -1,14 +1,26 @@
 #!/bin/env python
 
 import cv2, time
+import pygame
+pygame.mixer.init()
+pygame.mixer.music.load("audio/scream.wav")
 
+
+## # play scream sound once
+## pygame.mixer.music.play(0)
+
+# start video capture from webcam
 cap = cv2.VideoCapture(0)
 time.sleep(1)
-cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+face_cascade   = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+
+# return image as is, without alpha channel
+scary_girl_img = cv2.imread('scary-girl.jpg', -1)
 
 def detect(image):
-    #faces = cascade.detectMultiScale(img, 1.1, 2, cv2.cv.CV_HAAR_SCALE_IMAGE, (20,20), (200,200))
-    faces = cascade.detectMultiScale(image)
+    #faces = face_cascade.detectMultiScale(img, 1.1, 2, cv2.cv.CV_HAAR_SCALE_IMAGE, (20,20), (200,200))
+    faces = face_cascade.detectMultiScale(image)
+
     for _face in faces:
         cv2.rectangle(image, (_face[0], _face[1]), (_face[0]+_face[2], _face[1]+_face[3]), (255,255,255))
 
